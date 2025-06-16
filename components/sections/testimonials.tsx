70144b283577147f14fbd61f01e77f884690757c
sujan0629 <sujanbhatta0629@gmail.com>
2025-06-16 17:47:37 +0545

    testimonial update

--- components/sections/testimonials.tsx
+++ components/sections/testimonials.tsx
@@ -10,18 +10,17 @@ export default function Testimonials() {
         <HeaderSection
           label="Testimonials"
           title="What our clients are sharing."
-          subtitle="Discover the glowing feedback from our delighted customers
-            worldwide."
+          subtitle="Discover the glowing feedback from our delighted customers worldwide."
         />
 
-        <div className="column-1 space-y-5 gap-5 md:columns-2 lg:columns-3">
+        <div className="columns-1 gap-5 space-y-5 md:columns-2 lg:columns-3">
           {testimonials.map((item) => (
             <div className="break-inside-avoid" key={item.name}>
               <div className="relative rounded-xl border bg-muted/25">
                 <div className="flex flex-col px-4 py-5 sm:p-6">
                   <div>
                     <div className="relative mb-4 flex items-center gap-3">
-                      <span className="relative inline-flex shrink-0 size-16 items-center justify-center overflow-hidden rounded-full border">
+                      <span className="inline-flex relative shrink-0 size-16 items-center justify-center overflow-hidden rounded-full border">
                         <Image
                           src={item.image}
                           alt={item.name}
@@ -34,9 +33,7 @@ export default function Testimonials() {
                         <p className="text-sm font-semibold text-foreground">
                           {item.name}
                         </p>
-                        <p className="text-sm text-muted-foreground">
-                          {item.job}
-                        </p>
+                        <p className="text-sm text-muted-foreground">{item.job}</p>
                       </div>
                     </div>
                     <q className="text-muted-foreground">{item.review}</q>
